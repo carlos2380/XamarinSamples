fc68aba4c4b851c6390a2cdf92f9d285049b8371
carlos2380 <carlos.2380@hotmail.com>
2017-11-24 01:32:11 +0100

    ClickedButton

---
+++ ClickedButton/App1/App1/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class MainViewModel : BindableObject
+    {
+        private string _message;
+        private int _counter;
+        public ICommand ClicCommand {get{return new Command(ButtonClic); } }
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value; 
+                OnPropertyChanged("Message");
+            }
+        }
+
+        private void ButtonClic()
+        {
+            _counter++;
+            Message = string.Format("Botón pulsado {0} veces!", _counter);
+        }
+    }
+}
